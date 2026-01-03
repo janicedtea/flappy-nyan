7cd10d7a9b40927911a0958bafbc8feab3505185
janicedtea <janicesu2019@gmail.com>
2026-01-03 11:22:41 -0500

    forgor to commit

---
+++ main.gd
@@ -0,0 +1,47 @@
+extends Node
+
+var game_running : bool
+var game_over : bool
+var scroll
+var score
+const scroll_speed : int = 4
+var screen_size : Vector2i
+var ground_height : int
+var pipes : Array
+const pipe_delay : int = 100 
+const pipe_range : int = 200
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+func new_game():
+	#reset variables
+	game_running = false
+	game_over = false
+	score = 0
+	scroll = 0
+	$nyan.reset()
+
+func _input(event):
+	if game_over == false:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				if game_running == false:
+					start_game()
+				else:
+					if $nyan.flying:
+						$nyan.flap()
+
+func start_game():
+	game_running = true
+	$nyan.flying = true
+	$nyan.flap()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
---
+++ nyan.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const gravity : int = 1000
+const max_vel : int = 600
+const flap_speed : int = -500
+var flying : bool = false
+var falling : bool = false
+const start_pos = Vector2(100, 400)
+
+func _ready():
+	reset()
+	
+func reset():
+	falling = false
+	flying = false
+	position = start_pos
+	set_rotation(0)
+	
+func _physics_process(delta):
+	if flying or falling:
+		velocity.y += gravity * delta
+		if velocity.y > max_vel:
+			velocity.y = max_vel
+		if flying:
+			set_rotation(deg_to_rad(velocity.y * 0.05))
+			$AnimatedSprite2D.play()
+		elif falling:
+			set_rotation(PI/2)
+			$AnimatedSprite2D.stop()
+		move_and_collide(velocity * delta)
+	else:
+		$AnimatedSprite2D.stop()
+		
+func flap():
+	velocity.y = flap_speed
