32c0daa47b01b562d01b82f9c526e83676eac89b
janicedtea <janicesu2019@gmail.com>
2026-01-04 13:45:54 -0500

    pipes

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var pipe_scene: PackedScene
 var game_running : bool
 var game_over : bool
 var scroll
@@ -16,6 +17,7 @@ const pipe_range : int = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_window().size
+	ground_height = $ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game():
@@ -24,6 +26,9 @@ func new_game():
 	game_over = false
 	score = 0
 	scroll = 0
+	pipes.clear()
+	#starting pipes
+	generate_pipes()
 	$nyan.reset()
 
 func _input(event):
@@ -40,6 +45,8 @@ func start_game():
 	game_running = true
 	$nyan.flying = true
 	$nyan.flap()
+	#start pipe timer
+	$pipe_timer.start()
 
 
 
@@ -52,3 +59,22 @@ func _process(delta):
 			scroll = 0
 		#move ground
 		$ground.position.x = -scroll
+		#move pipes
+		for pipe in pipes:
+			pipe.position.x -= scroll_speed
+
+
+func _on_pipe_timer_timeout() -> void:
+	generate_pipes()
+	
+func generate_pipes():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + pipe_delay
+	@warning_ignore("integer_division")
+	pipe.position.y = (screen_size.y - ground_height) / 2 + randi_range(-pipe_range, pipe_range)
+	pipe.hit.connect(nyan_hit)
+	add_child(pipe)
+	pipes.append(pipe)
+
+func nyan_hit():
+	pass
